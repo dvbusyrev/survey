d57cb1aabfba712927510dc8facf00fd37623a7c
busyrev-dv <dima.busyr@yandex.ru>
2026-04-02 17:04:36 +0300

    Next step migration

--- wwwroot/js/core/http.js
+++ wwwroot/js/core/http.js
@@ -1,1 +1,29 @@
-(function(){async function requestJson(url,options){var response=await fetch(url,options||{});var data;try{data=await response.json();}catch(e){data={success:false,message:'Сервер вернул не JSON'};}if(!response.ok){throw new Error((data&&data.message)||('HTTP '+response.status));}return data;}window.Http=window.Http||{requestJson:requestJson,antiforgeryToken:function(){var tokenEl=document.querySelector('input[name="__RequestVerificationToken"]');return tokenEl?tokenEl.value:'';},jsonHeaders:function(){var token=this.antiforgeryToken();return Object.assign({'Content-Type':'application/json'},token?{'RequestVerificationToken':token}:{});}};})();
+(function(){
+  async function requestJson(url, options){
+    var response = await fetch(url, options || {});
+    var data;
+    try { data = await response.json(); } catch (e) { data = { success: false, message: 'Сервер вернул не JSON' }; }
+    if (!response.ok) {
+      throw new Error((data && data.message) || ('HTTP ' + response.status));
+    }
+    return data;
+  }
+
+  function antiforgeryToken(){
+    var tokenEl = document.querySelector('input[name="__RequestVerificationToken"]');
+    return tokenEl ? tokenEl.value : '';
+  }
+
+  function jsonHeaders(){
+    var token = antiforgeryToken();
+    var headers = { 'Content-Type': 'application/json' };
+    if (token) headers['RequestVerificationToken'] = token;
+    return headers;
+  }
+
+  window.Http = window.Http || {
+    requestJson: requestJson,
+    antiforgeryToken: antiforgeryToken,
+    jsonHeaders: jsonHeaders
+  };
+})();
