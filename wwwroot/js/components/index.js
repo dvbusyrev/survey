430e097f6f0672fa4a9371ce3ab7f6f9a9b8dbe9
busyrev-dv <dima.busyr@yandex.ru>
2026-04-02 14:01:37 +0300

    delete bad files

--- wwwroot/js/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Header } from './Header.js';
-export { default as Navigation } from './Navigation.js';
-export { default as Footer } from './Footer.js';
-
-import Layout from './Layout';
-
-export {
-    Layout
-}; 
