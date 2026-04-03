b94cb96b193556b81f64254b98de5e3a2f7a58db
busyrev-dv <dima.busyr@yandex.ru>
2026-04-03 10:53:46 +0300

    Next step

---
+++ wwwroot/js/pages/admin-archives.js
@@ -0,0 +1,33 @@
+window.AdminArchives = (function () {
+  function closeModalById(id) {
+    var modal = document.getElementById(id);
+    if (modal) modal.style.display = 'none';
+  }
+
+  function closeAnswersModal() {
+    closeModalById('answersModal');
+  }
+
+  function wireEscClose() {
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        closeAnswersModal();
+      }
+    });
+  }
+
+  function init() {
+    wireEscClose();
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+
+  return {
+    closeModalById: closeModalById,
+    closeAnswersModal: closeAnswersModal
+  };
+})();
