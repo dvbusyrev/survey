13dcfdec5e49ec751d17b403400e213eb8bed307
busyrev-dv <dima.busyr@yandex.ru>
2026-08-04 16:26:50 +0300

    fix error in name

--- Features/Surveys/SurveyService.AutoCreation.Helpers.cs
+++ Features/Surveys/SurveyService.AutoCreation.Helpers.cs
@@ -26,13 +26,13 @@ public partial class SurveyService
         if (!IsValidBusinessDayPeriod(request.ReportingOffsetBusinessDays))
         {
             return InvalidAutoCreationRequest(
-                "Поле «Срок подготвки отчёта» должно быть положительным целым числом.");
+                "Поле «Срок подготовки отчёта» должно быть положительным целым числом.");
         }
 
         if (!IsValidBusinessDayPeriod(request.ActivePeriodBusinessDays))
         {
             return InvalidAutoCreationRequest(
-                "Поле «Срок доступности анкеты» должно быть положительным целым числом.");
+                "Поле «Срок доступности анкет» должно быть положительным целым числом.");
         }
 
         var surveyIds = (request.SurveyIds ?? [])
